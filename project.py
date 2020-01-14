a64d644c4d4dde6534f2236fb9f7ec8fc9e4ddf3
orivera0508 <57488345+orivera0508@users.noreply.github.com>
2020-01-14 15:24:45 -0400

    Update: Phase 1
    Phase 1 has been updated to identify type: Float, Int, Variable (Valid),  Operational and Arithmetic Operators.

--- project.py
+++ project.py
@@ -1,63 +1,132 @@
-# Project: IMPLEMENTATION OF LAO COMPILER
-# Version 1.0
-# Author: Orlando J. Rivera Guevara
-# Date: 12/06/2019
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 14 09:57:40 2020
 
+@author: Orlando J. Rivera Guevara
+@description: Test Environment to identify identifyers:
+"""
+#Modules used:
 import re
 
-# Setting Error warning:
+#Initializing error_warning
 error_warning = ""
 
+print("PHASE 1: IDENTIFY TOKENS\n\n")
 
-# ##########################################################
-#               PHASE 1: Determining Variables:
-# ##########################################################
+#Getting User Input:
+user_input = input("CommandLine>")
 
-def badVar(variable):
+#Getting my tokens:
+tokens = user_input.split()
+
+###############################################################################
+#                           Step 1: Identify
+###############################################################################
+
+def identifyKeyWords(token):
+    if token == "print" or token == "rem" or token == "if" or token == "else" or token == "end":
+        return True
+    else:
+        return False
+
+def isValidIdentifier(token):
     #Accessing Global Variable:
     global error_warning
     
-    if re.search(r'''([0-9])\w+''', variable):
-        error_warning = "ERROR: Variables cannot start with numbers"
-        return True #bad command so returning True
-    elif re.search(r'''^\s+$''', variable):
-        error_warning = "ERROR: Variables annot contain white spaces"
-        return True #bad command so returning True
-    elif re.search(r'''[!@#$%^&*(),.?":{}|<>\/]''',variable):
-        error_warning = "ERROR: Varibale cannot contain special characters"
-        return True #bad command so returning True
-    else: 
-        return False #good command so returning False
-
-user_input = str(input('CommandLine>'))
-
-
-if badVar(user_input) is False:
-    print("Good Variable: " + user_input)
-elif badVar(user_input) is True:
-    print("BAD VARIABLE\n" + error_warning)
-
-
-# ##########################################################
-#               PHASE 2: Compiling Senetences:
-# ##########################################################
-
-# Now we determine sentences:
-
-def getCommands(variable):
-    result =  re.findall(r'''([^\s]+)''',variable)
+    #Commands used by Command Line:
+    #if re.search("cls", token):
+    #    os.system('cls')
+    #elif re.search("exit", token):
+    #    print("Good Bye")
+    #    os.system('exit')
     
-    #return len(result)
-    return result
+    if re.search(r'''([0-9])\w+''', token):
+        error_warning = "ERROR: Variables cannot start with numbers"
+        return False
+    elif re.search(r'''\.+\w+\.''', token):
+        error_warning = "ERROR: Unexpected function"
+        return False
+    elif re.search(r'''^\s+$''', token):
+        error_warning = "ERROR: Variables cannot contain white spaces"
+        return False
+    elif re.search(r'''[!@#$%^&*(),.?":{}|<>\/]''', token):
+        error_warning = "ERROR: Varibale cannot contain special characters"
+        return False
+    else: 
+        return True
 
-#print("Words: " + str(getCommands(user_input)))
-
-commands = getCommands(user_input)
-
-#Determining which commands are variables:
-
-for i in range(len(commands)):
-    if badVar(commands[i]) is False:
-        print(commands[i] + " is good")
+def isArithmeticOperator(token):
+    if token == ".add." or token == ".sub." or token == ".mul." or token == ".div.":
+        return True
     else:
-        print(commands[i] + " is bad")
+        return False
+    
+def isLogicalOperator(token):
+    if token == ".or." or token == ".and." or token == ".not." or token == ".eq." or token == ".ne." or token == ".lt." or token == ".le." or token == ".gt." or token == ".ge.":
+        return True
+    else:
+        return False
+
+#Only detects positive integers:
+def isInt(token):
+    if token.isdigit():
+        return True
+    else:
+        return False
+
+def isFloat(token):
+    if re.match(r'^-?\d+(?:\.\d+)?$', token) is None:
+        return False
+    else:
+        return True
+
+for token in tokens:
+    if identifyKeyWords(token) is True:
+        print("Token is a Keyword: " + token)
+    elif isInt(token) is True:
+        print("Token is an Int: " + token)
+    elif isFloat(token) is True:
+        print("Token is a Float: " + token)
+    elif isArithmeticOperator(token) is True:
+        print("Token is an Arithmetic Operator: " + token)
+    elif isLogicalOperator(token) is True:
+        print("Token is an Logical Operator: " + token)
+    elif isValidIdentifier(token) is True:
+        print("Token is a Valid Identifier: " + token)
+    else:
+        print("Unexpected Error: Cannot Identify this Token: " + token)
+
+###############################################################################
+#                           Step 2: Analyze
+###############################################################################
+
+
+
+
+
+
+
+
+
+###############################################################################
+#                           Step 3: Execute
+###############################################################################
+'''
+def lexicalAnalysis(list):
+    #print(list)
+    
+    for i in range(len(list)):
+    #Things that will prompt an error during lexical analysis:
+        if isArithmeticOperator(list[0]) is True or isLogicalOperator(list[0]) is True:
+            print(list[0])
+            print("ERROR: Invalid Syntax")
+            break
+     #This is my print statement:
+        elif identifyKeyWords(list[i]) is True and list[i] == "print" and '"' in list[i + 1]:
+            sentence = ""
+            for i in list[i + 1:]:
+                sentence =sentence + i.replace('"', '') + " "
+            print (sentence)
+
+lexicalAnalysis(tokens)
+'''
